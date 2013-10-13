ed38afea98962934b02a5b468a8fa4d3e0636d43
Parag Naik <parnaik@deloitte.com>
2013-10-13 20:54:40 +0530

    bobble head : changed onMeasure, added interpolator, added simple gesture.

--- Android/POCollections/src/com/cyberinsane/pocollections/customview/BobbleHeadView.java
+++ Android/POCollections/src/com/cyberinsane/pocollections/customview/BobbleHeadView.java
@@ -7,9 +7,15 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.MeasureSpec;
+import android.view.animation.AnimationUtils;
+import android.view.animation.CycleInterpolator;
 
 import com.cyberinsane.pocollections.R;
 
@@ -19,30 +25,30 @@ public class BobbleHeadView extends View {
 
     private Matrix transform = new Matrix();
     private Vector2D position = new Vector2D();
-    private TouchManager touchManager = new TouchManager(1);
 
     private boolean isInitialized = false;
 
     private int mWidth;
     private int mHeight;
-    private float mMoveXBy = 0;
-    private float mMoveYBy = 0;
-    private int mStifness = 2;
 
-    private float mOldX = 0;
-    private float mOldY = 0;
+    int mInterpolatorStartTime;
+    private CycleInterpolator mInterpolator;
+
+    private int mDuration = 2000;
+    private float mDurationReciprocal = 1 / (float) mDuration;
 
     private float mTotalDistance = 0;
-
-    private View self;
 
     private Bitmap doggieHead;
 
     Paint paint;
 
+    private GestureDetector mGestureDetector;
+
+    private int timePassed;
+
     public BobbleHeadView(Context context) {
         super(context);
-        self = this;
     }
 
     public BobbleHeadView(Context context, AttributeSet attrs, int defStyle) {
@@ -52,98 +58,39 @@ public class BobbleHeadView extends View {
 
     public BobbleHeadView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        self = this;
         doggieHead = BitmapFactory.decodeResource(getResources(), R.drawable.bullseye_head);
         mWidth = doggieHead.getWidth();
         mHeight = doggieHead.getHeight();
 
+        initDetectors(context);
+        mInterpolator = new CycleInterpolator(10f);
+
         paint = new Paint();
 
+    }
+
+    @SuppressWarnings("synthetic-access")
+    private void initDetectors(Context context) {
+
+        mGestureDetector = new GestureDetector(context, new MyGestureListener());
     }
 
     @SuppressLint("NewApi")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
-        // switch (event.getAction() & MotionEvent.ACTION_MASK) {
-        // case MotionEvent.ACTION_DOWN:
-        // mMoveXBy = event.getX();
-        // mMoveYBy = event.getY();
-        // break;
-        // case MotionEvent.ACTION_UP:
-        // break;
-        // case MotionEvent.ACTION_POINTER_DOWN:
-        // break;
-        // case MotionEvent.ACTION_POINTER_UP:
-        // break;
-        // case MotionEvent.ACTION_MOVE: {
-        // invalidate();
-        //
-        // moveDoggiHead(event);
-        // break;
-        // }
-        // }
-
-        touchManager.update(event);
-        position.add(touchManager.moveDelta(0));
-        invalidate();
-
-        return true;
+        boolean retVal = mGestureDetector.onTouchEvent(event);
+        return retVal || super.onTouchEvent(event);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
-        setMeasuredDimension(mWidth, mHeight);
-    }
+        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.getSize(heightMeasureSpec));
 
-    public float getDistance(float startX, float startY, MotionEvent ev) {
-        float distanceSum = 0;
-        final int historySize = ev.getHistorySize();
-        for (int h = 0; h < historySize; h++) {
-            // historical point
-            float hx = ev.getHistoricalX(0, h);
-            float hy = ev.getHistoricalY(0, h);
-            // distance between startX,startY and historical point
-            float dx = (hx - startX);
-            float dy = (hy - startY);
-            distanceSum += Math.sqrt(dx * dx + dy * dy);
-            // make historical point the start point for next loop iteration
-            startX = hx;
-            startY = hy;
-        }
-        // add distance from last historical point to event's point
-        float dx = (ev.getX(0) - startX);
-        float dy = (ev.getY(0) - startY);
-        distanceSum += Math.sqrt(dx * dx + dy * dy);
-        return distanceSum;
     }
 
     @SuppressLint("NewApi")
-    private void moveDoggiHead(MotionEvent ev) {
-
-        // final float dx = ev.getX() - mOldX;
-        // final float dy = ev.getY() - mOldY;
-        // final float dl = (float) Math.sqrt(dx * dx + dy * dy);
-        // mTotalDistance += dl;
-        // mOldX = ev.getX();
-        // mOldY = ev.getY();
-
-        // ViewPropertyAnimator animatorX =
-        // com.nineoldandroids.view.ViewPropertyAnimator.animate(self).translationX(ev.getX()).setDuration(0);
-        // ViewPropertyAnimator animatory =
-        // com.nineoldandroids.view.ViewPropertyAnimator.animate(self).translationY(ev.getY()).setDuration(0);
-
-        // System.out.println("HUHA :" + mTotalDistance);
-        // invalidate();
-        // requestLayout();
-
-        mMoveXBy = ev.getX();
-        mMoveYBy = ev.getY();
-        invalidate();
-
-    }
-
     @Override
     protected void onDraw(Canvas c) {
 
@@ -155,10 +102,42 @@ public class BobbleHeadView extends View {
         }
 
         transform.reset();
-        transform.postTranslate(-mWidth / 2.0f, -mHeight / 2.0f);
-        transform.postTranslate(position.getX() / mStifness, position.getY() / mStifness);
+        transform.postTranslate((getWidth() - mWidth) / 2, (getHeight() - mHeight) / 2);
+        
+        timePassed = (int) (AnimationUtils.currentAnimationTimeMillis() - mInterpolatorStartTime);
+
+        if (timePassed < mDuration) {
+
+            shakeItShakeItDoggie();
+        }
 
         c.drawBitmap(doggieHead, transform, paint);
+    }
+    
+    private void shakeItShakeItDoggie() {
+        
+        float x = timePassed * mDurationReciprocal;
+        x = mInterpolator.getInterpolation(x);
+
+        transform.postRotate(x * 200f / timePassed, getWidth() / 2 + (x*100f), getHeight() / 2 + (x*100f));
+
+        invalidate();
+    }
+
+    private class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+
+            return DoggieAnimStart();
+        }
+
+        private boolean DoggieAnimStart() {
+            mInterpolatorStartTime = (int) AnimationUtils.currentAnimationTimeMillis();
+            postInvalidate();
+            return true;
+        }
+
     }
 
 }
